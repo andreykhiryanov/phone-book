e017737b087a99ddee5b9bcb2e94382d34d947f8
Andrey Khiryanov <andreykhiryanov@gmail.com>
2019-11-21 21:14:41 +0300

    First run

---
+++ src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void firstButtonAction (ActionEvent actionEvent) {
+        System.out.println("First button!");
+    }
+
+    public void secondButtonAction (ActionEvent actionEvent) {
+        System.out.println("Second button!");
+    }
+
+}
