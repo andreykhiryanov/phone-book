2dfa739192dc0e6b8ef6df366c56bbb99a670987
Andrey Khiryanov <andreykhiryanov@gmail.com>
2019-11-26 13:03:05 +0300

    New window and ids

---
+++ src/com/generation/brain/phonebook/controller/AddController.java
@@ -0,0 +1,26 @@
+package com.generation.brain.phonebook.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddController {
+
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtSurname;
+    @FXML
+    private TextField txtPhoneNumber;
+    @FXML
+    private TextField txtAddress;
+    @FXML
+    private TextField txtAge;
+    @FXML
+    private TextField txtNote;
+    @FXML
+    private Button btnAdd;
+    @FXML
+    private Button btnCancel;
+
+}
